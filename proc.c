881dadde201d058a2ea13e0c5a3898b92e0b2b14
emanifesto <emmanuelbabayemi37@gmail.com>
2025-11-03 01:09:45 -0500

    Learned never to include a .c file inside a .c and added prototype to defs.h file to grant mappages declaration to proc.c

--- proc.c
+++ proc.c
@@ -1,12 +1,11 @@
 #include "types.h"
 #include "defs.h"
 #include "param.h"
-// #include "memlayout.h"
-// #include "mmu.h"
-// #include "x86.h"
-// #include "proc.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
 #include "spinlock.h"
-#include "vm.c"
 
 struct {
   struct spinlock lock;
