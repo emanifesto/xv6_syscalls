790782404fffa5feec3de88748ce8bf1decc9acf
emanifesto <emmanuelbabayemi37@gmail.com>
2025-11-03 02:14:50 -0500

    Defined join and altered wait(); unsure if copyout in join returns user stack pointer to the correct address but everything else should be fine

--- proc.c
+++ proc.c
@@ -381,9 +381,58 @@ wait(void)
     // Scan through table looking for exited children.
     havekids = 0;
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->parent != curproc)
+      if(p->parent != curproc || p->pgdir == curproc->pgdir)
         continue;
       havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
+// Wait for a cloned process to exit and return its pid.
+// Return -1 if this process has no clones.
+int
+join(void)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited clones.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc || p->pgdir != curproc->pgdir)
+        continue;
+      havekids = 1;
+
+      uint sp = (uint)p->userstack + PGSIZE; // Top of the stack page
+      // Returns the user stack pointer of the cloned process
+      if(copyout(p->pgdir, sp, p->userstack, (uint)sizeof(p->userstack)) < 0) return -1;
+      
       if(p->state == ZOMBIE){
         // Found one.
         pid = p->pid;
